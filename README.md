8fe355bbef5a5d2f9442c8033a7ffc4d856de436
jhenoc <jenocutn@gmail.com>
2021-01-09 09:04:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+COTIZADOR DE COSTO DE PINTURA
+
+El objetivo de esta herramienta es calcular el costo de pintura de tubos y accesorio de acero. 
+
+Para obtener el precio, se debera seleccionar el diametro de la tuberia, indicar la cantidad de accesorios correspondientes(codo 90°,
+codo 45°, reducción, Te y bridas) y el largo de la tuberia. 
+
+El proceso de cálculo que se llevará a cabo 
---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+/*N° para cada diametro
+1	 1/2"
+2	 3/4"
+3	1"
+4	1 1/4"
+5	1 1/2"
+6	2"
+7	2 1/2"
+8	3"
+9	4"
+10	5"
+11	6"
+12	8"
+13	10"
+14	12"
+15	14"
+16	16"
+17	18"
+18	20"
+19	24"
+*/
+
+const price = 10;//Dolares
+
+const factorElbow90 = [0.030,0.045,0.060,0.075,0.090,0.120,0.150,0.180,0.240,0.300,0.360,0.480,0.600,0.720,0.840,0.960,1.080,1.200,1.450];//cada accesorio tiene una factor por cada diametro de tubo. Posteriormente se cargaran los factores para otros accesorios
+const factorElbow45 = [0.015,0.023,0.030,0.038,0.045,0.060,0.075,0.090,0.120,0.150,0.180,0.240,0.300,0.360,0.420,0.480,0.540,0.600,0.725];
+
+
+class Painting{
+constructor(diameter, elbow90, elbow45, te, reduction, flange, lengthTube){
+    this.diameter = diameter;
+    this.elbow90 = elbow90;
+    this.elbow45 = elbow45;
+    this.te = te;
+    this.reduction = reduction;
+    this.lengthTube = lengthTube;
+}
+    diameter = 0;
+    calculateAreaTotal (){
+        let elbow90Area = this.elbow90 * factorElbow90[this.diameter];
+        let elbow45Area = this.elbow45 * factorElbow45[this.diameter];
+        return elbow90Area + elbow45Area;
+    }
+
+    calculateTotalPrice(){
+        let totalPrice = price * this.calculateAreaTotal;
+        return totalPrice;
+}
+}
+
+/* Test para un accesorio de codo de 90
+let area1 = new Painting(1);
+let total = area1.calculateAreaAcc();
+console.log(total);
+*/
