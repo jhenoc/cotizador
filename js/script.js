104dd4a158b45f3dba4d1948f48a184e7befd3a7
jhenoc <jenocutn@gmail.com>
2021-01-23 23:48:05 -0300

    Desafio 4

--- js/script.js
+++ js/script.js
@@ -23,6 +23,15 @@
 
 const price = 10;//Dolares
 
+function getSelectedDiameter ()
+{
+    const selectDiameter = document.getElementById("inputGroupSelect02").value;
+    localStorage.setItem('diameter',selectDiameter);
+}
+                                                                                                                                                               
+
+
+
 const factorElbow90 = [0.030,0.045,0.060,0.075,0.090,0.120,0.150,0.180,0.240,0.300,0.360,0.480,0.600,0.720,0.840,0.960,1.080,1.200,1.450];//cada accesorio tiene una factor por cada diametro de tubo. Posteriormente se cargaran los factores para otros accesorios
 const factorElbow45 = [0.015,0.023,0.030,0.038,0.045,0.060,0.075,0.090,0.120,0.150,0.180,0.240,0.300,0.360,0.420,0.480,0.540,0.600,0.725];
 
@@ -36,18 +45,18 @@ constructor(diameter, elbow90, elbow45, te, reduction, flange, lengthTube){
     this.reduction = reduction;
     this.lengthTube = lengthTube;
 }
-    diameter = 0;
+                
     calculateAreaTotal (){
-        let elbow90Area = this.elbow90 * factorElbow90[this.diameter];
-        let elbow45Area = this.elbow45 * factorElbow45[this.diameter];
-        return elbow90Area + elbow45Area;
-    }
+        let elbow90Area = this.elbow90 * factorElbow90[localStorage.getItem('diameter')];
+        return elbow90Area;
 
+    }
     calculateTotalPrice(){
         let totalPrice = price * this.calculateAreaTotal;
         return totalPrice;
 }
-}
+    }
+
 
 /* Test para un accesorio de codo de 90
 let area1 = new Painting(1);
